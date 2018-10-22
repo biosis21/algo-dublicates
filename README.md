f8356b0da948b8876e96f4105ea99a398273892b
Mykola Savenko <biosis21@gmail.com>
2018-10-22 21:51:15 +0300

    add function for generate numbers and find dublicates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Algorithm Duplicates
+
+## Installation
+
+Before running the script please check the version of Node and NPM. It should corresponds to the package.json `engines` block.
+
+## How to run
+
+```
+node index.js n=5
+```
+
+## Tests
+
+:)
---
+++ index.js
@@ -0,0 +1,55 @@
+// Parse input arguments
+const args = process.argv
+    .slice(2)
+    .map(arg => arg.split('='))
+    .reduce((args, [value, key]) => {
+        args[value] = key;
+        return args;
+    }, {});
+
+
+const n = parseInt(args.n, 10) || 1000000;
+console.log('N =', n);
+
+printDuplicates(generateList(n));
+
+/**
+ * Generate random number
+ * @param {number} n
+ * @returns {Array}
+ */
+function generateList(n) {
+    const arr = [];
+
+    for (let i = 0; i < n; i++) {
+        arr.push(Math.floor(Math.random() * (n - 1) + 1));
+    }
+
+    // Tricky case 'Therefore there must be at least one duplicate in the list.'.
+    // Just add a first element to the end of array
+    arr.push(arr[0]);
+
+    return arr;
+}
+
+/**
+ * Function that find and print duplicate numbers.
+ * @param {Array} list
+ */
+function printDuplicates(list) {
+    console.log('Generated list =', list);
+
+    // Collection for the saving unique numbers
+    const numbers = {};
+    let output = '';
+
+    list.forEach((el) => {
+        if (numbers[el]) {
+            output += '\'' + el + '\' ';
+        } else {
+            numbers[el] = true;
+        }
+    });
+
+    console.log(output);
+}
